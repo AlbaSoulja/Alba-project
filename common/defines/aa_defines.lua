f912207997f0ecdda49452ec2cb9beb41aa42fa9
RoachySalad <112738131+RoachySalad@users.noreply.github.com>
2022-09-03 04:47:59 +0200

    Add files via upload

---
+++ common/defines/aa_defines.lua
@@ -0,0 +1,3 @@
+NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.5, 0.15, 0.05, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 999
